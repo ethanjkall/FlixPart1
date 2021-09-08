1592e605ba3749292ec2d452b6fc00617b666da8
ethanjkall <65936657+ethanjkall@users.noreply.github.com>
2021-09-08 19:38:49 -0400

    Initial Commit

---
+++ FlixPart1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlixPart1
+//
+//  Created by Ethan Kallarackal on 9/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
